018d3591bd2979206176dbc01ce5af829a4acea6
Atonim <Atonimaurus@gmail.com>
2023-07-02 17:37:40 +0300

    fix: base routing

---
+++ main_front/src/containers/Notfound.js
@@ -0,0 +1,8 @@
+import React from 'react'
+const Notfound = (props) =>(
+        <div>
+            OOPSIE-DOOPSIE THIS PAGE WAS NOT FOUND :(
+        </div>
+);
+
+export default Notfound;
--- main_front/src/hocs/Layout.js
+++ main_front/src/hocs/Layout.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-const Layout = (props) =>(
+import {Outlet} from "react-router-dom";
+import './Layout.css'
+
+const Layout = (props) =>{
+    return (
         <div>
+            <header className='header'>
             <Navbar/>
             {props.children}
+        </header>
+        <main className='main'>
+            <Outlet/>
+        </main>
+        <footer className='footer'>
+            2023
+        </footer>
         </div>
-);
+
+    )
+};
 
 export default Layout;
